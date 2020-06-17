c411da5cde7a56ebe1c95221df5be0ab6927db18
神崎航 <kanzwataru@gmail.com>
2020-06-17 19:39:38 -0400

    * add piano polyphony

--- src/test_piano.cpp
+++ src/test_piano.cpp
@@ -1,4 +1,5 @@
 #include <cstdint>
+#include <cstdlib>
 #include <SDL2/SDL.h>
 #include "main_test.h"
 #include "extern/imgui/imgui.h"
@@ -37,17 +38,17 @@ static const uint16_t freq_table[N_TOTAL] = {
 
 static const char *note_name_table[N_TOTAL] = {
     [N_CS]    = "C#",
-    [N_D]     = "D",
+    [N_D]     = "D ",
     [N_DS]    = "D#",
-    [N_E]     = "E",
-    [N_F]     = "F",
+    [N_E]     = "E ",
+    [N_F]     = "F ",
     [N_FS]    = "F#",
-    [N_G]     = "G",
+    [N_G]     = "G ",
     [N_GS]    = "G#",
-    [N_A]     = "A",
+    [N_A]     = "A ",
     [N_AS]    = "A#",
-    [N_B]     = "B",
-    [N_C]     = "C"
+    [N_B]     = "B ",
+    [N_C]     = "C "
 };
 
 static int keyboard_table[N_TOTAL] = {
@@ -64,10 +65,6 @@ static int keyboard_table[N_TOTAL] = {
     [N_B]     = SDL_SCANCODE_M,
     [N_C]     = SDL_SCANCODE_COMMA
 };
-
-static bool initialized = false;
-static int  prev_note_held = -1;
-static uint8_t reg_B0 = 0;
 
 struct Operator {
     uint8_t attack;
@@ -94,7 +91,14 @@ struct Instrument {
     }
 };
 
+struct NoteInfo {
+    int note = -1;
+    uint8_t reg_b0 = 0;
+};
+
+static bool initialized = false;
 static Instrument instrument;
+static NoteInfo prev_notes[9];
 
 static void registers_clear(void)
 {
@@ -103,22 +107,27 @@ static void registers_clear(void)
     }
 }
 
+static uint8_t register_of(uint8_t addr, uint8_t voice, uint8_t op)
+{
+    static const uint8_t offsets[2][9] = {
+        {0x00, 0x01, 0x02, 0x08, 0x09, 0x0A, 0x10, 0x11, 0x12}, // OP 1
+        {0x03, 0x04, 0x05, 0x0B, 0x0C, 0x0D, 0x13, 0x14, 0x15}  // OP 2
+    };
+
+    return addr + offsets[op][voice];
+}
+
 static void instrument_set(const Instrument &instr, int voice)
 {
-    adlib_out(0x20 + voice, 0x01);
-    adlib_out(0x40 + voice, 0x2A);
+    adlib_out(register_of(0x20, voice, 0), 0x01);
+    adlib_out(register_of(0x40, voice, 0), 0x2A);
+    adlib_out(register_of(0x60, voice, 0), (instr.mod.attack << 4) | instr.mod.decay);
+    adlib_out(register_of(0x80, voice, 0), (instr.mod.sustain << 4) | instr.mod.release);
 
-    //adlib_out(0x60 + voice, 0xF0);
-    //adlib_out(0x80 + voice, 0x77);
-    adlib_out(0x60 + voice, (instr.mod.attack << 4) | instr.mod.decay);
-    adlib_out(0x80 + voice, (instr.mod.sustain << 4) | instr.mod.release);
-
-    adlib_out(0x23 + voice, 0x01);
-    adlib_out(0x43 + voice, 0x00);
-
-    //adlib_out(0x63 + voice, 0xF0);
-    adlib_out(0x63 + voice, (instr.car.attack << 4) | instr.car.decay);
-    adlib_out(0x83 + voice, (instr.car.sustain << 4) | instr.car.release);
+    adlib_out(register_of(0x20, voice, 1), 0x01);
+    adlib_out(register_of(0x40, voice, 1), 0x00);
+    adlib_out(register_of(0x60, voice, 1), (instr.car.attack << 4) | instr.car.decay);
+    adlib_out(register_of(0x80, voice, 1), (instr.car.sustain << 4) | instr.car.release);
 }
 
 #if 0
@@ -153,7 +162,7 @@ static void dummy_instrument_set()
 static void stop(int voice)
 {
     //adlib_out(0xB0 + voice, 0x00);
-    adlib_out(0xB0 + voice, reg_B0 & 0x1F);
+    adlib_out(0xB0 + voice, prev_notes[voice].reg_b0 & 0x1F);
 }
 
 static void play(int voice, unsigned short octave, unsigned short note)
@@ -162,7 +171,7 @@ static void play(int voice, unsigned short octave, unsigned short note)
     unsigned char msb = (1 << 5) | ((7 & octave) << 2) | ((0xFF00 & note) >> 8);
     adlib_out(0xA0 + voice, lsb);
     adlib_out(0xB0 + voice, msb);
-    reg_B0 = msb;
+    prev_notes[voice].reg_b0 = msb;
     //printf("[%d] octave: %u note: %u lsb: 0x%2X msb: 0x%2X\n", voice, octave, note, lsb, msb);
 }
 
@@ -174,25 +183,62 @@ void test_piano()
         initialized = true;
     }
 
-    //for(int i = 0; i < 4; ++i) {
-    //    instrument_set(instrument, i);
-    //}
-    instrument_set(instrument, 0);
+    for(int i = 0; i < 9; ++i) {
+        instrument_set(instrument, i);
+    }
 
 
     SDL_PumpEvents();
     const uint8_t *keys = SDL_GetKeyboardState(nullptr);
-    int note_held = -1;
+    struct NoteInfo notes[9];
+    memcpy(notes, prev_notes, sizeof(notes));
+
+    auto find = [&](int n) -> int {
+        for(int i = 0; i < 9; ++i) {
+            if(notes[i].note == n) {
+                return i;
+            }
+        }
+
+        return -1;
+    };
 
     for(int i = 0; i < N_TOTAL; ++i) {
         if(keys[keyboard_table[i]]) {
-            note_held = i;
+            int note_idx = find(i);
+            if(note_idx == -1) {
+                int free_idx = find(-1);
+                if(free_idx != -1) {
+                    notes[free_idx].note = i;
+                }
+            }
         }
     }
-    const char *key_label = note_held == -1 ? "_" : note_name_table[note_held];
+
+    for(int i = 0; i < 9; ++i) {
+        if(!keys[keyboard_table[notes[i].note]]) {
+            notes[i].note = -1;
+        }
+    }
+
+    for(int i = 0; i < 9; ++i) {
+        if(notes[i].note != prev_notes[i].note) {
+            if(notes[i].note != -1) {
+                play(i, 4, freq_table[notes[i].note]);
+            }
+            else {
+                stop(i);
+            }
+        }
+
+        prev_notes[i].note = notes[i].note;
+    }
+
+    auto label_of = [&](int i) { return notes[i].note == -1 ? "__" : note_name_table[notes[i].note]; };
 
     ImGui::Begin("Piano");
-    ImGui::LabelText("Keys Held", ": %s", key_label);
+    ImGui::LabelText("Keys Held", ": %s %s %s %s %s %s %s %s %s",
+                     label_of(0), label_of(1), label_of(2), label_of(3), label_of(4), label_of(5), label_of(6), label_of(7), label_of(8));
 
     const uint8_t single_min = 0x0;
     const uint8_t single_max = 0xF;
@@ -215,14 +261,8 @@ void test_piano()
     ImGui::PopID();
     ImGui::End();
 
-    if(note_held != prev_note_held) {
-        if(note_held != -1) {
-            play(0, 4, freq_table[note_held]);
-        }
-        else {
-            stop(0);
-        }
+    printf("\n");
+    for(int i = 0; i < 9; ++i) {
+        printf("%d %d\n", prev_notes[i].note, notes[i].note);
     }
-
-    prev_note_held = note_held;
 }
